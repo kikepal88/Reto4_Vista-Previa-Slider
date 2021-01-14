a88154dc22ff06de434c94c0a987a826a0109e99
Omar Palacios <omarpal88@gmail.com>
2021-01-13 21:59:18 -0500

    Listos los estilos de absulotamente todo y el JS para generar sliders

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function showSlider1 () {
+  document.getElementById("slider1").classList.toggle("show");
+  document.getElementById("slider2").classList.toggle("show");
+};
+
+function showSlider2 () {
+  document.getElementById("slider2").classList.toggle("show");
+  document.getElementById("slider1").classList.toggle("show");
+};
